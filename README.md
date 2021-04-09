19a499bdafe8c2a7df9c13c787f681356ca343e1
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2021-04-09 09:25:54 +0530

    free standing rust binary

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rust Os
+
+## Minimal OS implementation in rust
+
+### For running
+
+```dotnetcli
+$ cargo build --target thumbv7em-none-eabihf
+```
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std] // disable standard library
+#![no_main] // for using different entry point than main function.
+
+use core::panic::PanicInfo;
+
+// Invoked when panic occurs
+// PanicInfo contains file, line and where it happened and optional panic message
+// diverging functions (returns !(never(not allowed to ever return)) type)
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+// overwriting entry point
+// `no mangle` to ensure rust compiler outputs function with _start() name and not _Zasdasd_start_asdadbvsd
+// we need function name because linker need the name of starting point
+// entry point is not called by any function, but invoked directly by the operating system or bootloader. 
+// Instead of returning, the entry point should e.g. invoke the exit system call of the operating system. 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // entry point
+    //linker looks for a function named `_start` by default
+    loop {}
+}
