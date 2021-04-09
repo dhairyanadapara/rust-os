275f829268553b3c71a62c5cca0b7033b97456af
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2021-04-09 23:45:38 +0530

    minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,25 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
 
 // overwriting entry point
 // `no mangle` to ensure rust compiler outputs function with _start() name and not _Zasdasd_start_asdadbvsd
 // we need function name because linker need the name of starting point
-// entry point is not called by any function, but invoked directly by the operating system or bootloader. 
-// Instead of returning, the entry point should e.g. invoke the exit system call of the operating system. 
+// entry point is not called by any function, but invoked directly by the operating system or bootloader.
+// Instead of returning, the entry point should e.g. invoke the exit system call of the operating system.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // entry point
     //linker looks for a function named `_start` by default
+    let vga_buffer = 0x0b8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
